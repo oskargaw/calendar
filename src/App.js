b1150cd210325db22b4be20386d56d4c1ea1b3df
Oskar Gawlak <oskar.gawlak@gmail.com>
2019-10-28 20:37:21 +0100

    Changed structure of the files tree

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Homepage from "./pages/Homepage";
+import Homepage from "./pages/Homepage/Homepage";
 
 import "./App.scss";
 
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import CardEventInfo from "../components/CardEventInfo/CardEventInfo";
-import CardCalendar from "../components/CardCalendar/CardCalendar";
+import CardEventInfo from "../../components/CardEventInfo/CardEventInfo";
+import CardCalendar from "../../components/CardCalendar/CardCalendar";
 
 import "./Homepage.scss";
 
