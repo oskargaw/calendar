d97ec7146572e3d5eacc5fe8e7c087bc842a4a01
Oskar Gawlak <oskar.gawlak@gmail.com>
2019-10-28 20:01:35 +0100

    Refactored card event info to the separate component

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,16 +1,11 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faClock,
-  faMapMarkerAlt,
-  faGlobeAmericas
-} from "@fortawesome/free-solid-svg-icons";
+import { faGlobeAmericas } from "@fortawesome/free-solid-svg-icons";
 
 import Card from "../components/Card/Card";
+import CardEventInfo from "../components/CardEventInfo/CardEventInfo";
 
 import { daysInSeptember, daysNames, hours } from "../data/daysInSeptember";
-
-import webSummitLogo from "../img/web-summit-logo.png";
 
 import "./Homepage.scss";
 
@@ -25,34 +20,7 @@ const Homepage = () => {
 
   return (
     <div className="homepage">
-      <Card section="event-info">
-        <img src={webSummitLogo} alt="Web Summit Logo" className="card__logo" />
-        <div className="card__company-name">WebSummit</div>
-        <h1 className="card__event-name">Local Meetup</h1>
-        <div className="card__event-details">
-          <div className="card__event-details-row">
-            <div className="card__event-details-icon-container">
-              <FontAwesomeIcon
-                icon={faClock}
-                className="card__event-details-icon"
-              />
-            </div>
-            <span>1 hr</span>
-          </div>
-          <div className="card__event-details-row">
-            <div className="card__event-details-icon-container">
-              <FontAwesomeIcon
-                icon={faMapMarkerAlt}
-                className="card__event-details-icon"
-              />
-            </div>
-            <span>Lisbon, Portugal</span>
-          </div>
-        </div>
-        <div className="card__event-description">
-          Local meetup for Entrepreneurs
-        </div>
-      </Card>
+      <CardEventInfo />
 
       <Card section="calendar">
         <h1 className="card__title">Select a Date & Time</h1>
